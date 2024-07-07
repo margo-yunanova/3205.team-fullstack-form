46e12aa144ad96ce91193fecd3bb6f3b35af2ad4
Margo <m.yunanova@gmail.com>
2024-07-07 23:35:43 +0300

    front: feat: add components on main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import { Form } from './components/Form';
+import { UsersTable } from './components/UsersTable/UsersTable';
+import { Stack } from '@mui/material';
+
+export interface IUserData {
+  email: string;
+  number: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<IUserData[]>([]);
+
   return (
-    <>
-      <Form />
-    </>
+    <Stack display="flex" flexDirection="column" gap="30px">
+      <Form setUsers={setUsers} />
+      <UsersTable users={users} />
+    </Stack>
   );
 }
 

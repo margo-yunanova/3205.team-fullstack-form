77c148885035b465651550f99e2d2c80ccef3603
Margo <m.yunanova@gmail.com>
2024-07-09 22:32:04 +0300

    back: refactor: extract filterUsers from controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { celebrate, Joi, errors, Segments } from 'celebrate';
-import { getUsers } from './controllers/users';
+import { filterUsers } from './controllers/users';
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -13,11 +13,20 @@ app.get(
   '/api/v1/user',
   celebrate({
     [Segments.QUERY]: {
-      email: Joi.string().email().required(),
+      email: Joi.string().required(),
       number: Joi.string(),
     },
   }),
-  getUsers,
+  async (req, res) => {
+    const { email, number } = req.query;
+
+    const users = await filterUsers(
+      String(email),
+      number ? String(number) : null,
+    );
+
+    res.send(users);
+  },
 );
 
 app.use(errors());
--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,16 +1,12 @@
-import { RequestHandler } from 'express';
 import db from '../db/db.json';
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-export const getUsers: RequestHandler = async (req, res) => {
-  const { email, number } = req.query;
-
-  const users = db.filter(
-    (user) =>
-      user.email === email && (number === undefined || user.number === number),
-  );
-
+export const filterUsers = async (email: string, number: string | null) => {
   await delay(5000);
-  res.send(users);
+
+  return db.filter(
+    (user) =>
+      user.email === email && (number === null || user.number === number),
+  );
 };
